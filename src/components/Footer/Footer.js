50c3c2c9011296eb47a983264779bde1191e901e
Lautaro Conte <lauty.conte@gmail.com>
2022-02-14 16:27:40 -0300

    subo ta-te-ti

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import './Footer.css'
+export default function Footer() {
+    return (
+        <div className='footer'>
+            <div className="footer__container">
+                <div className="info-contact">
+                    <h1 className="footer__title">conte</h1>
+                <span className="footer__copy">
+                    &#169;Copyright <script>document.write(new Date().getFullYear())</script>,
+                     <span className="footer__title"><b>Conte</b></span>. All rights reserved.
+                </span>
+            </div>
+        </div>
+        </div>
+
+        // falta terminar
+    )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css'
+export default function Header() {
+    return (
+        <div className='header'>
+            <h1 className='text'>TA-TE-TI</h1>
+        </div>
+        
+    )
+}
